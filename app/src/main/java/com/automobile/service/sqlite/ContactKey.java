813c919d199e4f2598f406f43b03407b3f1f08d1
mehul <mehultank12@gmail.com>
2019-06-28 09:22:19 +0530

    Initial commit

---
+++ app/src/main/java/com/automobile/service/sqlite/ContactKey.java
@@ -0,0 +1,46 @@
+package com.dyrct.app.sqlite.db;
+
+public class ContactKey 
+{
+	
+	public static String ID = "id";
+	public static String USER_ID = "userid";
+	public static String ADDRESS_ID = "address_id";
+	public static String CONTACT_ID = "contact_id";
+	public static String TYPE_ADDRESS = "type";
+	public static String FIRST_NAME = "firstName";
+	public static String LAST_NAME = "lastname";
+	public static String COMPANY_NAME = "companyname";
+	public static String DESIGNATION = "designation";
+	public static String EMAIL = "email";
+	public static String PHONE = "phone";
+	public static String ALTERNATE_PHONE = "alternate_phone";
+	public static String URL = "url";
+	public static String ADDRESS = "address";
+	public static String ZIPCODE = "zipcode";
+	public static String COUNTRY = "country";
+	public static String STATE = "state";
+	public static String CITY = "city";
+	public static String LAT = "lat";
+	public static String LONG = "long";
+	public static String CATEGORY = "category";
+	public static String PROFILE_IMAGE = "profileimage";
+	public static String PROFILE_IMAGE_ID = "profileimageid";
+	public static String IS_DYRCT_USER = "isDyrctUser";
+	public static String TIMESTAMP = "timestamp";
+	public static String NOTE = "note";
+    public static String NOTE_PUBLIC = "note_public";
+    public static String APP_VERSION = "app_version";
+	public static String DEFAULT_ADDRESS = "default_address";
+	public static String PROFILE_VISIBILTY_FLAG = "profileVisibiltyFlag";
+	
+
+//	public static String PRIVATE_ADDRESS = "privateaddress";
+//	public static String PRIVATE_ZIPCODE = "privatezipcode";
+//	public static String PRIVATE_COUNTRY = "privatecountry";
+//	public static String PRIVATE_STATE = "vstate";
+//	public static String PRIVATE_CITY = "privatecity";
+//	public static String PRIVATE_LAT = "privatelat";
+//	public static String PRIVATE_LONG = "privatelong";
+
+}
